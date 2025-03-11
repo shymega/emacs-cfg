5bcfc144293c04fc2db502c71b7dff545c22453a
Dom Rodriguez <shymega@shymega.org.uk>
2025-03-11 18:25:37 +0000

    feat: Initial commit

---
+++ conf/emacs/.lock/flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "THIS IS AN AUTO-GENERATED FILE. PLEASE DON'T EDIT IT MANUALLY.";
+  inputs = {
+    copilot = {
+      flake = false;
+      owner = "copilot-emacs";
+      repo = "copilot.el";
+      type = "github";
+    };
+    dash = {
+      flake = false;
+      owner = "magnars";
+      repo = "dash.el";
+      type = "github";
+    };
+    "f" = {
+      flake = false;
+      owner = "rejeep";
+      repo = "f.el";
+      type = "github";
+    };
+    "s" = {
+      flake = false;
+      owner = "magnars";
+      repo = "s.el";
+      type = "github";
+    };
+    zenburn-theme = {
+      flake = false;
+      owner = "bbatsov";
+      repo = "zenburn-emacs";
+      type = "github";
+    };
+  };
+  outputs = {...}: {};
+}
